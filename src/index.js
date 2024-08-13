72478a13c72ed034fa0fc321a9806bdf8fe781be
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-08-13 16:36:20 +0200

    Create classi "Node" e "LinkedList". Creati metodi "append" e "prepend" per aggiungere  elementi a inizio o fine lista.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("linked");
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.list = null;
+  }
+
+  append = (value, list = this.list) => {
+    if (list.nextNode) {
+      this.append(value, list.nextNode);
+    } else {
+      list.nextNode = new Node(value);
+    }
+  };
+
+  prepend = (value) => {
+    const newNode = new Node(value, this.list);
+    this.list = newNode;
+  };
+
+  size = (value) => {};
+}
+
+const list = new LinkedList();
+list.prepend(5);
+list.prepend(2);
+list.prepend(6);
+console.log(list.list);
+list.append(4);
+list.append(2);
+list.append(8);
